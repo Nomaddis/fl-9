e6265c679d272a13a6417bc2d8d4d753c4da4dbe
Nomaddis <nomaddis@gmail.com>
2018-08-14 22:23:50 +0300

    done 6.1

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,1 +1,17 @@
 // Your code goes here
+//TODO round 0.00 = > 0
+function calc() {
+    let inputAmount = prompt("Please enter amount of money");
+    let inputDiscount = prompt("Please enter your discount");
+    if(inputAmount < 0 || inputDiscount < 0) {
+        console.log("Invalid data");
+    } else {
+        console.log(
+            `Price without discount: ${inputAmount.toFixed(2)} 
+Discount: ${inputDiscount.toFixed(2)}%
+Price with discount: ${(inputAmount*(1-(inputDiscount/100))).toFixed(2)}
+Saved:  ${(inputAmount*(inputDiscount/100)).toFixed(2)}`
+        );
+    }
+}
+calc();
