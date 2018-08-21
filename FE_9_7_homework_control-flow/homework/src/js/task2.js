e9c8edb3ed678d21b3ac67d925af7b80d817fe74
Nomaddis <nomaddis@gmail.com>
2018-08-21 22:18:54 +0300

    DONE 7.2

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,40 +1,51 @@
 // Your code goes here
+
+const MAX = 5;
+const MIN_PRIZE = 10;
+const ONE = 1;
 let start = confirm('Do you want to play a game?');
 if(!start) {
     alert('You did not become a millionaire, but can.');
 } else {
-    startGame(0, 5, 10);
+    startGame(0, MAX, MIN_PRIZE);
 }
-let totalPrize = 0;
-
-function startGame(min = 0, max, prize = 10, totalPrize = 0) {
+function startGame(min = 0, max, prize = MIN_PRIZE, totalPrize = 0) {
     let iNum = getRandom(min, max);
     console.log(iNum);
-    var possiblePrize = prize;
-    for(let i = 3; i>0; i--) {
-        console.log(possiblePrize + "poss before");
-        let uNum = prompt(`Enter your number from ${min} to ${max}
-         Attempts left: ${i}
-         Total prize: ${totalPrize}$
-         Possible prize on current attempts ${possiblePrize}$
-        `);
-        if(+uNum === iNum && i === 3) {
+    let possiblePrize = prize;
+    for(let i = 3; i>0; i--, possiblePrize=Math.floor(possiblePrize/2)) {
+        let uNum = prompt(
+`Enter your number from ${min} to ${max}
+Attempts left: ${i}
+Total prize: ${totalPrize}$
+Possible prize on current attempts ${possiblePrize}$`);
+        if(uNum === iNum && i === 3) {
             totalPrize += possiblePrize;
-            alert(`Congratulation!   Your prize is:  $${possiblePrize}`);
-            startGame(min, max*2, prize*2, totalPrize);
-            break;
-        } else if(+uNum === iNum && i === 2) {
+            if(confirm(`Congratulation!   Your prize is:  $${possiblePrize} Do you want to continue?`)) {
+                startGame(min, max*2, prize*2, totalPrize);
+                break;
+            } else {
+                alert(`Thank you for a game. Your prize is: ${totalPrize}`);
+                break;
+            }
+        } else if(uNum === iNum && i === 2) {
             possiblePrize = Math.floor(prize/2);
             totalPrize = possiblePrize + totalPrize;
-            alert(`Congratulation!   Your prize is:  $${possiblePrize}`);
-            startGame(min, max*2, prize*2, totalPrize);
-            break;
-        } else if(+uNum === iNum && i === 1) {
-            possiblePrize = Math.floor(prize/2);
+            if(confirm(`Congratulation!   Your prize is:  $${possiblePrize} Do you want to continue?`)) {
+                startGame(min, max*2, prize*2, totalPrize);
+                break;
+            } else {
+                alert(`Thank you for a game. Your prize is: ${totalPrize}`);
+            }
+        } else if(uNum === iNum && i === ONE) {
+            possiblePrize = Math.floor(prize/4);
             totalPrize = possiblePrize + totalPrize;
-            alert(`Congratulation!   Your prize is:  $${possiblePrize}`);
-            startGame(min, max*2, prize*2, totalPrize);
-            break;
+            if(confirm(`Congratulation!   Your prize is:  $${possiblePrize} Do you want to continue?`)) {
+                startGame(min, max*2, prize*2, totalPrize);
+                break;
+            } else {
+                alert(`Thank you for a game. Your prize is: ${totalPrize}`);
+            }
         } else {
             alert(`Thank you for a game. Your prize is: ${totalPrize}`);
         }
@@ -44,5 +55,5 @@ function startGame(min = 0, max, prize = 10, totalPrize = 0) {
 function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Включаючи мінімум та максимум
+    return Math.floor(Math.random() * (max - min + ONE)) + min;
 }
