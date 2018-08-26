2d64051553856a6f66c549759659d892a31ad9b6
Nomaddis <nomaddis@gmail.com>
2018-08-26 16:26:34 +0300

    Fix closest to zero

--- FE_9_8_homework_functions/homework/src/getClosestToZero.js
+++ FE_9_8_homework_functions/homework/src/getClosestToZero.js
@@ -1,8 +1,7 @@
 function getClosestToZero() {
-    let i=0;
-    let minDiff=1000;
+    let minDiff = Infinity;
     let ans;
-    for(i in arguments){
+    for(let i = 0; i < arguments.length; i++){
         let m=Math.abs(0-arguments[i]);
         if(m<minDiff){
             minDiff=m;
