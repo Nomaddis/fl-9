de54ec1e448a26c14ebcc91e0f3ea625b4008ae1
Nomaddis <nomaddis@gmail.com>
2018-09-04 22:05:44 +0300

    add 11HW

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,24 +1,28 @@
-// var rootNode = document.getElementById("root");
-//
-// // Your code goes here
-//
-// rootNode.appendChild(/* Append your list item node*/);
-
-
 let addTaskItem = document.getElementById('addItem');
 let taskItemList = document.getElementById('main-list');
-let checkboxTask = document.getElementById('checkbox-task');
-
 let listItemEdit = document.getElementById('listitem-edit');
+let qtyOfItems = 0;
+const MAX_SIZE_OF_TASKS = 10;
+const ONE = 1;
+const MINUS_ONE = 1;
+const ZERO = 0;
+let dragSrcEl = null;
 
-function updateFrom(button) {
-    let element = document.getElementById('main-list');
-    // let num = button.parentNode.querySelector("li");
+function deleteNode(button) {
     let num = getClosest(button, 'li');
-    console.log(element);
-    console.log(num);
-    element.parentNode.removeChild(num);
+    num.remove();
 }
+
+function changeItem(e) {
+    console.log(e.target.nodeName);
+    if (e.target.nodeName === 'I' && e.target.id !== 'checkbox-task' ) {
+        deleteNode(e.target);
+        qtyOfItems = qtyOfItems - ONE;
+    } else if(e.target.id === 'checkbox-task'){
+        changeCheckboxStatus(e.target);
+    }
+}
+
 function getClosest(elem, selector) {
     if (!Element.prototype.matches) {
         Element.prototype.matches =
@@ -30,56 +34,109 @@ function getClosest(elem, selector) {
             function(s) {
                 let matches = (this.document || this.ownerDocument).querySelectorAll(s),
                     i = matches.length;
-                while (--i >= 0 && matches.item(i) !== this) {}
-                return i > -1;
+                while (--i >= ZERO && matches.item(i) !== this) {
+                    //
+                }
+                return i > MINUS_ONE;
             };
     }
-    // Get the closest matching element
     for ( ; elem && elem !== document; elem = elem.parentNode ) {
-        if ( elem.matches( selector ) ) return elem;
+        if ( elem.matches( selector ) ) {
+            return elem;
+        }
     }
+
     return null;
-
 }
-
-
-
-function deleteTaskItem(e) {
-    if (e.target.nodeName === "BUTTON" || e.target.nodeName === "I") {
-        updateFrom(e.target);
-        console.log(e.target);
-    }
-}
-
-
 
 function addTask(event) {
     let userInput = document.getElementById('taskDescription').value;
     document.getElementById('taskDescription').value = '';
-    if(userInput.length === 0) {
-        userInput.innerHTML = "Please fill in the form!";
-    } else {
+    if(userInput.length === ZERO) {
+        alert('Please fill in the form!');
+    } else if(qtyOfItems < MAX_SIZE_OF_TASKS) {
         let newTask = `
               <div id="main-list__item" class="listitem main-list__item">
                 <div class="listitem-group">
-                  <div id="listitem-checkox"><i class="material-icons">check_box_outline_blank</i></div>
+                  <div id="listitem-checkox">
+                      <i class="material-icons" id="checkbox-task">
+                      check_box_outline_blank
+                      </i>
+                  </div>
                   <div class="listitem-text"><span>${userInput}</span></div>
                 </div>
                 <button id="listitem-edit"><i class="material-icons">delete</i></button>
               </div>
         `;
         let listItem = document.createElement('li');
+        listItem.setAttribute('draggable', true);
         listItem.innerHTML = newTask;
         document.getElementById('main-list').appendChild(listItem);
+        qtyOfItems = qtyOfItems + ONE;
+    } else if(qtyOfItems === MAX_SIZE_OF_TASKS) {
+        alert('Maximum item per list are created');
     }
+    let cols = document.querySelectorAll('#main-list li');
+    [].forEach.call(cols, addDnDHandlers);
+
     event.preventDefault();
 }
 
-function changeCheckboxStatus(event) {
-    event.srcElement.innerHTML = 'check_box';
+function changeCheckboxStatus(elem) {
+    elem.innerHTML = 'check_box';
 }
 
-addTaskItem.addEventListener("click", addTask, false);
-checkboxTask.addEventListener("click", changeCheckboxStatus, false);
-taskItemList.addEventListener("click", deleteTaskItem, false);
-// listItemEdit.addEventListener("click", deleteTaskItem, false);
+addTaskItem.addEventListener('click', addTask , false);
+taskItemList.addEventListener('click', changeItem, false);
+
+function handleDragStart(e) {
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.outerHTML);
+  this.classList.add('dragElem');
+}
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  this.classList.add('over');
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDragEnter(e) {
+//
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl !== this) {
+    this.parentNode.removeChild(dragSrcEl);
+    let dropHTML = e.dataTransfer.getData('text/html');
+    this.insertAdjacentHTML('beforebegin',dropHTML);
+    let dropElem = this.previousSibling;
+    addDnDHandlers(dropElem);
+    
+  }
+  this.classList.remove('over');
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.classList.remove('over');
+}
+
+function addDnDHandlers(elem) {
+  elem.addEventListener('dragstart', handleDragStart, false);
+  elem.addEventListener('dragenter', handleDragEnter, false)
+  elem.addEventListener('dragover', handleDragOver, false);
+  elem.addEventListener('dragleave', handleDragLeave, false);
+  elem.addEventListener('drop', handleDrop, false);
+  elem.addEventListener('dragend', handleDragEnd, false);
+}
